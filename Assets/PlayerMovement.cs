06ab05e25a01ac0a4db78ab8ae916cc02a7cdb60
Denis Onder <denis_onder@protonmail.com>
2020-06-01 05:29:19 +0200

    Load project files to repo
    Load project files to repo

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public Rigidbody rb;
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    rb.AddForce(0, 200, 500 * Time.deltaTime);
+  }
+}
