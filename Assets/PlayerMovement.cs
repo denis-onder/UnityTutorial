033faf366c27baf635a95b8e695b6ba873f15dd5
Denis Onder <denis_onder@protonmail.com>
2020-06-01 05:38:50 +0200

    Added input handling for movement
    Added input handling for movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,6 +3,8 @@
 public class PlayerMovement : MonoBehaviour
 {
   public Rigidbody rb;
+  public float forwardForce = 2000f;
+  public float lateralForce = 500f;
   // Start is called before the first frame update
   void Start()
   {
@@ -12,6 +14,16 @@ public class PlayerMovement : MonoBehaviour
   // Update is called once per frame
   void FixedUpdate()
   {
-    rb.AddForce(0, 200, 500 * Time.deltaTime);
+    rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+    if(Input.GetKey("d"))
+    {
+    rb.AddForce(lateralForce * Time.deltaTime, 0, 0);
+    }
+
+    if(Input.GetKey("a"))
+    {
+    rb.AddForce(-lateralForce * Time.deltaTime, 0, 0);
+    }
   }
 }
